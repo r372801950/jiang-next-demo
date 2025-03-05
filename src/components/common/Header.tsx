843ce9c9e38e6900e336b88746ace24c5ecb30e2
jiang <372801950@qq.com>
2025-03-06 01:34:13 +0800

    添加钱包余额

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -33,32 +33,13 @@ const Header = () => {
               <span>首页</span>
             </a>
             <a
-              href="/dapp"
+              href="/blog"
               className="flex items-center text-gray-600 hover:text-gray-900 transition-colors"
             >
               <Wallet className="w-5 h-5 mr-1" />
-              <span>DApp</span>
+              <span>Blog</span>
             </a>
           </nav>
-
-           {/*右侧钱包连接按钮 */}
-          {/*<div className="flex items-center">*/}
-          {/*  <button*/}
-          {/*    onClick={handleConnectWallet}*/}
-          {/*    className={`*/}
-          {/*      flex items-center px-4 py-2 rounded-lg*/}
-          {/*      transition-colors duration-200*/}
-          {/*      ${*/}
-          {/*        isWalletConnected*/}
-          {/*          ? "bg-green-50 text-green-700 hover:bg-green-100"*/}
-          {/*          : "bg-blue-600 text-white hover:bg-blue-700"*/}
-          {/*      }*/}
-          {/*    `}*/}
-          {/*  >*/}
-          {/*    <Wallet className="w-5 h-5 mr-2" />*/}
-          {/*    <span>{isWalletConnected ? "已连接钱包" : "连接钱包"}</span>*/}
-          {/*  </button>*/}
-          {/*</div>*/}
           <div className="flex items-center">
             <ConnectKitButton />
           </div>
